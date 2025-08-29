cf7eccaedab4dc23c2aed5e94a4978b869be1c17
mdsohanmondal <mdshohanmondal@gmail.com>
2025-08-29 21:20:03 +0600

    add password system

--- 3.java/Hello.java
+++ 3.java/Hello.java
@@ -1,6 +1,4 @@
-
 import java.util.Scanner;
-
 
 public class Hello {
     public static void main(String[] args){
@@ -14,6 +12,15 @@ public class Hello {
         }else{
             System.out.println("Not allowed");
             System.out.println("User will be age 18+ or 18");
-        }        
+        }
+        
+        String password = "sohan1234";
+        String logPass;
+        logPass = sc.next();
+        if(logPass.equals(password)){
+            System.out.println("Login Success");
+        }else{
+            System.out.println("your password was incorrect");
+        }
     }
 }
